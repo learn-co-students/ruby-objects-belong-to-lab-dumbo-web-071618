e1df35d2b8b4f0daf5e100ee4ac311202995603a
Jason Pitts <juniorjr083@gmail.com>
2018-07-22 03:26:17 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+    attr_accessor :title 
+    attr_reader :author
+   
+    def initialize()
+      @title = title 
+   
+    end 
+
+
+    def author=(value)
+        @author = value
+
+
+    end 
+   
+   
+   
+   
+   end 
